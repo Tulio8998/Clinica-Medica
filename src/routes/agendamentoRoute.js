2fe34adc9be55f50bada61681e09740a95138abf
tulio <tulio89lopes@gmail.com>
2026-01-13 16:36:44 -0300

    feat: adiciona rotas de agendamentos do medico

--- src/routes/agendamentoRoute.js
+++ src/routes/agendamentoRoute.js
@@ -1,14 +1,22 @@
 const { Router } = require('express');
-const agendamentoController = require('../controllers/RecepcionistaController/AgendamentoController');
+const agendamentoController = require('../controllers/RecepcionistaController/RecepcionistaAgendamentoController');
+const medicoController = require('../controllers/MedicoController/MedicoAgendamentoController');
 const authRecep = require('../middlewares/authRecepcionista');
+const authMedic = require('../middlewares/authMedico');
 
 const routes = new Router();
 
-routes.post('/agendamento', authRecep, agendamentoController.create);
-routes.get('/agendamento', agendamentoController.list);
-routes.get('/agendamento/buscar', agendamentoController.select);
-routes.put('/agendamento', authRecep, agendamentoController.update);
-routes.delete('/agendamento', agendamentoController.delete);
+routes.post('/agendamento/recepcionista', authRecep, agendamentoController.create);
+routes.get('/agendamento/recepcionista', agendamentoController.list);
+routes.get('/agendamento/recepcionista/buscar', agendamentoController.select);
+routes.put('/agendamento/recepcionista', authRecep, agendamentoController.update);
+routes.delete('/agendamento/recepcionista', agendamentoController.delete);
+
+
+routes.get('/agendamento/medico', medicoController.list);
+routes.get('/agendamento/medico/buscar', medicoController.select);
+routes.put('/agendamento/medico', authMedic, medicoController.update);
+
 
 
 module.exports = routes;
